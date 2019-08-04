1a0f34015803352ef3722ab2b1599cb166160001
cmitchell <clmitchell630@gmail.com>
2019-08-03 22:21:38 -0500

    ALL more styling. JS buttons work, appending new buttons works, click event to add gifs works. Prolly should have committed earlier...

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,25 +7,70 @@
 
 //------code------
 $(function () {
+
     //buttons
     var topics = ["Cats", "Ducks", "Mangos", "Ryan Gosling", "Will Ferrell", "Pharrell Williams", "Prickly Pear", "Red Panda", "Sneeze", "Laughing"];
+    //initialize buttons
+    for (var i = 0; i < topics.length; i++) {
+        addButton(topics[i]);
+    }
+    //add topic button
+    $("#topic-add").on("click", growTopics);
 
-    topicButtons();
     //------functions------
-    function topicButtons(){
-        for(var i = 0; i < topics.length; i++){
-            var newBtn = $("<button>");
-            newBtn.attr("type", "button");
-            newBtn.addClass("btn btn-secondary");
-            newBtn.text(topics[i]);
-            $("#gifBtns").append(newBtn);
-        }
+    function addButton(name) {
+
+        var newBtn = $("<button>");
+        newBtn.attr("type", "button");
+        newBtn.addClass("btn btn-secondary addBtnStyle");
+        newBtn.text(name);
+        $("#gifBtns").append(newBtn);
+        $(newBtn).on("click", getGif);
+
     }
 
     function growTopics(event) {
         event.preventDefault();
 
         var newTopic = $("#topic-search").val().trim();
-        
+        topics.push(newTopic);
+
+        addButton(newTopic);
+
+        $("#topic-search").val("");
+    }
+
+    function getGif(){
+        var gifTopic = $(this).text();
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+        gifTopic + "&api_key=9dffnf22zOjtVp4Y7PhLeiTV3bf0mYUN&limit=10";
+
+        $.ajax({
+            url:queryURL,
+            method:"GET"
+        }).then(function(response) {
+            console.log(response);
+
+            var results = response.data;
+
+            for(var i = 0; i < results.length; i++){
+                var gifDiv = $("<div>");
+                var p = $("<div>").addClass("ratingStyle").text("Rating: " + results[i].rating);
+
+                //setting up the gif image element
+                var gifImage = $("<img>");
+                gifImage.attr("src", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-still", results[i].images.fixed_height_still.url);
+                gifImage.attr("data-animate", results[i].images.fixed_height.url);
+                gifImage.attr("data-state", "still");
+                gifImage.addClass("gif");
+
+                gifDiv.addClass("imageStyle");
+                gifDiv.append(p);
+                gifDiv.append(gifImage);
+
+                $("#gifGoHere").prepend(gifDiv);
+            }
+        })
     }
 });
